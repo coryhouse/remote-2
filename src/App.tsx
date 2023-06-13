3bc31a6a62f3b910698ac12f62a336ea8110dafe
Cory House <housecor@gmail.com>
2023-06-13 12:31:38 -0500

    Refactor

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import { Box } from "@chakra-ui/react";
-import CounterAppTwo from "./components/CounterAppTwo";
+import RemoteAppTwo from "./components/RemoteAppTwo";
+import { useNavigate } from "react-router-dom";
 
 export default function App() {
+  const navigate = useNavigate();
   return (
     <Box margin="1.2rem">
       <Box>APP-2</Box>
       <Box>
-        <CounterAppTwo />
+        <RemoteAppTwo parentCount={0} nav={navigate} />
       </Box>
     </Box>
   );
