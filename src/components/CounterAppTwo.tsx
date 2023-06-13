56e297abb14dd1292eb582e032d566349c891104
Cory House <housecor@gmail.com>
2023-06-13 11:48:53 -0500

    Add props

--- src/components/CounterAppTwo.tsx
+++ src/components/CounterAppTwo.tsx
@@ -2,7 +2,12 @@ import { Button, Flex, Text } from "@chakra-ui/react";
 import React, { useState } from "react";
 import { Link, useLocation } from "react-router-dom";
 
-export default function Counter() {
+type RemoteProps = {
+  parentCount: number;
+  nav: (url: string) => void;
+};
+
+export default function CounterAppTwo({ nav, parentCount }: RemoteProps) {
   const [count, setCount] = useState(1);
 
   const location = useLocation();
@@ -10,6 +15,7 @@ export default function Counter() {
   return (
     <Flex color="#000" gap="1rem" direction="column">
       <Text>Click count : {count} </Text>
+      <Text>Parent count: {parentCount} </Text>
       <Button onClick={() => setCount((prevState) => prevState + 1)}>
         Click me
       </Button>
