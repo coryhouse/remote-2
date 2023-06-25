4f53348fbac3ea79e4323a1aa415f3e80329d980
Cory House <housecor@gmail.com>
2023-06-25 10:54:46 -0500

    remove chakra

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,13 @@
-import { Box } from "@chakra-ui/react";
 import RemoteTwo from "./components/RemoteTwo";
 
 export default function App() {
   return (
-    <Box margin="1.2rem">
-      <Box>REMOTE-2</Box>
-      <Box>
-        <RemoteTwo
-          baseUrl=""
-          parentCount={0}
-          user={{ id: 2, name: "Bobby" }}
-          urls={{ about: "/about", home: "/" }}
-        />
-      </Box>
-    </Box>
+    // Hard coding these props for local dev of this remote in isolation. These props are provided by the shell in the production app. */
+    <RemoteTwo
+      baseUrl=""
+      parentCount={0}
+      user={{ id: 2, name: "Bobby" }}
+      urls={{ about: "/about", home: "/" }}
+    />
   );
 }
