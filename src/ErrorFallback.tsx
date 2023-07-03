d39b39168f413d3e86cdcd8a13759f904db93f83
Cory House <housecor@gmail.com>
2023-07-03 08:56:07 -0500

    Tweak message

--- src/ErrorFallback.tsx
+++ src/ErrorFallback.tsx
@@ -4,7 +4,7 @@ import { FallbackProps } from "react-error-boundary";
 export function ErrorFallback({ error, resetErrorBoundary }: FallbackProps) {
   return (
     <div role="alert">
-      <p>Oops! This is the custom Remote 2 error fallback.</p>
+      <p>This is the custom Remote 2 error fallback.</p>
       <pre style={{ color: "red" }}>{error.message}</pre>
       <button onClick={resetErrorBoundary}>Try again</button>
     </div>
