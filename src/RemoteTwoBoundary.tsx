77759853b7d4d0a560824ede8653511666a40059
Cory House <housecor@gmail.com>
2023-07-04 08:14:36 -0500

    add

---
+++ src/RemoteTwoBoundary.tsx
@@ -0,0 +1,19 @@
+import { ErrorBoundary } from "react-error-boundary";
+import RemoteTwo from "./RemoteTwo";
+import { ErrorFallback } from "./ErrorFallback";
+
+type RemoteTwoBoundaryProps = {
+  baseUrl: string;
+  shellCount: number;
+  account: string;
+  urls: Record<"about" | "home", string>;
+  user: { id: number; name: string };
+};
+
+export default function RemoteTwoBoundary(props: RemoteTwoBoundaryProps) {
+  return (
+    <ErrorBoundary FallbackComponent={ErrorFallback}>
+      <RemoteTwo {...props} />
+    </ErrorBoundary>
+  );
+}
