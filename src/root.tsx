617dc24a1e40faec95dfbec4bf95de1d02de68c5
Cory House <housecor@gmail.com>
2023-07-04 08:14:23 -0500

    call boundary

--- src/root.tsx
+++ src/root.tsx
@@ -1,13 +1,14 @@
 import { createRoot } from "react-dom/client";
 import RemoteTwo from "./RemoteTwo";
 import { ShellContext } from "./ShellContext";
+import RemoteTwoBoundary from "./RemoteTwoBoundary";
 
 // This root is used for running the remote by itself.
 // The shell provides a context, so we wrap our remote in the shell's context to run it alone.
 // The shell also passes props to the remotes, so we must do the same here.
 createRoot(document.getElementById("root") as HTMLElement).render(
   <ShellContext>
-    <RemoteTwo
+    <RemoteTwoBoundary
       baseUrl=""
       shellCount={0}
       account="fake-account"
